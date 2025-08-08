e1e3136ddd2434e7b6e341f4c272f9a6f8d9ec1f
Dev Dhanuka <lohandanuka@gmail.com>
2025-08-08 11:27:46 +0530

    did some navbar updates

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,16 +1,24 @@
 'use client';
 
 import { FileUser} from "lucide-react";
+import { Smooch } from "next/font/google";
 
 const Navbar = () => {
   return (
     <header className="fixed top-0 left-0 w-full z-50 backdrop-blur-md bg-black/30 border-b border-gray-800">
       <div className="max-w-7xl mx-auto px-6 sm:px-12 py-4 flex items-center justify-between">
         {/* Logo */}
-        <div className="flex items-center gap-2">
+          <div 
+           onClick={(e) => {
+            e.preventDefault();
+            document.getElementById('hero')?.scrollIntoView({ behavior: 'smooth' });
+          }}
+
+          className="flex items-center gap-2 cursor-pointer">
           <FileUser className="w-6 h-6 text-yellow-500 " />
           <span className="font-bold text-xl text-white bg-gradient-to-r from-orange-500 to-yellow-400 bg-clip-text">ResumeAI</span>
         </div>
+        
 
         {/* Navigation Links */}
         <nav className="hidden md:flex items-center gap-6 text-lg text-gray-300">
@@ -20,7 +28,11 @@ const Navbar = () => {
         </nav>
 
         {/* Get Started Button */}
-        <button className="hidden font-semibold md:block bg-gradient-to-r from-orange-500 to-yellow-400 text-black px-4 py-2 rounded-md text-md hover:opacity-90 transition">
+        <button
+        onClick={() => {
+          document.getElementById('upload')?.scrollIntoView({behavior:'smooth'});
+        }}
+        className="hidden font-semibold md:block bg-gradient-to-r from-orange-500 to-yellow-400 text-black px-4 py-2 rounded-md text-md hover:opacity-90 transition">
           Get Started
         </button>
       </div>
